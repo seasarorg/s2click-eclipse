970bb739ee3969c737c94ef95b3c96470462da6b
takezoe <takezoe@gmail.com>
2010-03-22 13:52:54 +0000

    不要なコードをコメントアウト。
    git-svn-id: https://www.seasar.org/svn/sandbox/s2click/trunk/eclipse@565 595bf13c-eed3-438f-ae63-4794fb747ba6

--- org.seasar.s2click.clickide.extension/src/org/seasar/s2click/clickide/extension/S2ClickPlugin.java
+++ org.seasar.s2click.clickide.extension/src/org/seasar/s2click/clickide/extension/S2ClickPlugin.java
@@ -1,17 +1,5 @@
 package org.seasar.s2click.clickide.extension;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.URL;
-
-import org.apache.click.eclipse.ClickUtils;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IWorkspaceRoot;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -47,7 +35,7 @@ public class S2ClickPlugin extends AbstractUIPlugin {
 	 */
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
-		removeResources();
+//		removeResources();
 		super.stop(context);
 	}
 
@@ -60,68 +48,68 @@ public class S2ClickPlugin extends AbstractUIPlugin {
 		return plugin;
 	}
 	
-	/** ブランクプロジェクトのテンポラリファイル */
-	private File blankZip = null;
+//	/** ブランクプロジェクトのテンポラリファイル */
+//	private File blankZip = null;
+//	
+//	/** S2JDBCを使うブランクプロジェクトのテンポラリファイル */
+//	private File blankS2JdbcZip = null;
+//	
+//	/**
+//	 * プロジェクトの生成に必要なリソースをテンポラリフォルダにコピーします。
+//	 */
+//	private void copyResources() throws IOException {
+//		if(blankZip == null){
+//			{
+//				blankZip = File.createTempFile("s2click-blank", ".zip");
+//				URL url = S2ClickPlugin.getDefault().getBundle().getEntry("resources/s2click-blank.zip");
+//				ClickUtils.copyStream(url.openStream(), new FileOutputStream(blankZip));
+//			}
+//			{
+//				blankS2JdbcZip = File.createTempFile("s2click-s2jdbc-blank", ".zip");
+//				URL url = S2ClickPlugin.getDefault().getBundle().getEntry("resources/s2click-s2jdbc-blank.zip");
+//				ClickUtils.copyStream(url.openStream(), new FileOutputStream(blankS2JdbcZip));
+//			}
+//		}
+//	}
 	
-	/** S2JDBCを使うブランクプロジェクトのテンポラリファイル */
-	private File blankS2JdbcZip = null;
+//	/**
+//	 * テンポラリフォルダにコピーしたリソースを削除します。
+//	 */
+//	private void removeResources(){
+//		if(blankZip != null){
+//			blankZip.delete();
+//			blankS2JdbcZip.delete();
+//		}
+//	}
 	
-	/**
-	 * プロジェクトの生成に必要なリソースをテンポラリフォルダにコピーします。
-	 */
-	private void copyResources() throws IOException {
-		if(blankZip == null){
-			{
-				blankZip = File.createTempFile("s2click-blank", ".zip");
-				URL url = S2ClickPlugin.getDefault().getBundle().getEntry("resources/s2click-blank.zip");
-				ClickUtils.copyStream(url.openStream(), new FileOutputStream(blankZip));
-			}
-			{
-				blankS2JdbcZip = File.createTempFile("s2click-s2jdbc-blank", ".zip");
-				URL url = S2ClickPlugin.getDefault().getBundle().getEntry("resources/s2click-s2jdbc-blank.zip");
-				ClickUtils.copyStream(url.openStream(), new FileOutputStream(blankS2JdbcZip));
-			}
-		}
-	}
-	
-	/**
-	 * テンポラリフォルダにコピーしたリソースを削除します。
-	 */
-	private void removeResources(){
-		if(blankZip != null){
-			blankZip.delete();
-			blankS2JdbcZip.delete();
-		}
-	}
-	
-	/**
-	 * S2Clickプロジェクトを作成します。
-	 * 
-	 * @param projectName プロジェクト名
-	 * @param useS2Jdbc S2JDBCを使うかどうか
-	 */
-	public void createProject(String projectName, boolean useS2Jdbc,
-			IProgressMonitor monitor) throws IOException, CoreException {
-		if(monitor == null){
-			monitor = new NullProgressMonitor();
-		}
-		
-		monitor.beginTask("S2Clickプロジェクトを作成中...", 3);
-		
-		copyResources();
-		monitor.worked(1);
-		
-		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-		IProject project = root.getProject(projectName);
-		project.create(monitor);
-		
-		// TODO リソースをプロジェクトにコピー
-		
-		project.open(monitor);
-		
-		monitor.worked(1);
-		
-		monitor.done();
-	}
+//	/**
+//	 * S2Clickプロジェクトを作成します。
+//	 * 
+//	 * @param projectName プロジェクト名
+//	 * @param useS2Jdbc S2JDBCを使うかどうか
+//	 */
+//	public void createProject(String projectName, boolean useS2Jdbc,
+//			IProgressMonitor monitor) throws IOException, CoreException {
+//		if(monitor == null){
+//			monitor = new NullProgressMonitor();
+//		}
+//		
+//		monitor.beginTask("S2Clickプロジェクトを作成中...", 3);
+//		
+//		copyResources();
+//		monitor.worked(1);
+//		
+//		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+//		IProject project = root.getProject(projectName);
+//		project.create(monitor);
+//		
+//		// TODO リソースをプロジェクトにコピー
+//		
+//		project.open(monitor);
+//		
+//		monitor.worked(1);
+//		
+//		monitor.done();
+//	}
 
 }
